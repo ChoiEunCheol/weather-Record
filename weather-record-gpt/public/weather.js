7755d9e6a363811837f171e0278914f9f937b426
최은철 <wndqhrgkdlel@gmail.com>
2023-10-24 16:01:54 +0900

    오류 처리 코드 추가

--- weather-record-gpt/public/weather.js
+++ weather-record-gpt/public/weather.js
@@ -13,13 +13,17 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(apiUrl)
         .then((response) => response.json())
         .then((data) => {
-            // 날씨 정보를 가져온 후 처리
-            const weatherInfo = document.getElementById("weather-info");
-            const temperature = Math.round(data.main.temp - 273.15); // 온도를 섭씨로 변환
-            const description = data.weather[0].description;
+            // API 응답 데이터 검사
+            if (data.main && data.weather && data.weather.length > 0) {
+                const weatherInfo = document.getElementById("weather-info");
+                const temperature = Math.round(data.main.temp - 273.15); // 온도를 섭씨로 변환
+                const description = data.weather[0].description;
 
-            const weatherText = `현재 온도: ${temperature}°C, 날씨: ${description}`;
-            weatherInfo.textContent = weatherText;
+                const weatherText = `현재 온도: ${temperature}°C, 날씨: ${description}`;
+                weatherInfo.textContent = weatherText;
+            } else {
+                console.error("날씨 정보를 파싱하는 중 오류가 발생했습니다.");
+            }
         })
         .catch((error) => {
             console.error("날씨 정보를 불러오는 중 오류가 발생했습니다.", error);
