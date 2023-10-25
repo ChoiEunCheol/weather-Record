418d8c1a4e2d1e2e5f18613b727adcabdcc32fc1
최은철 <wndqhrgkdlel@gmail.com>
2023-10-25 16:27:01 +0900

    시작시간을 10초단위로

--- weather-record-gpt/weather.js
+++ weather-record-gpt/weather.js
@@ -50,6 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         const temperature = Math.round(weatherData.main.temp - 273.15);
 
                         const now = new Date();
+                        const seconds = now.getSeconds();
 
                         // 데이터 배열에 시간과 온도 추가
                         data.push({ time: now.toLocaleString(), temperature });
@@ -78,19 +79,19 @@ document.addEventListener("DOMContentLoaded", () => {
                         if (weatherList.children.length >= 168) {
                             weatherList.removeChild(weatherList.lastChild);
                         }
-
-                        const li = document.createElement("li");
-                        li.textContent = `갱신 시각: ${now.toLocaleString()}, ${weatherText}`;
-                        weatherList.insertBefore(li, weatherList.firstChild);
                     })
                     .catch((error) => {
                         console.error("날씨 정보를 갱신하는 중 오류가 발생했습니다.", error);
                     });
             }
 
-            updateWeatherData();
-
-            setInterval(updateWeatherData, 10000); // 10초마다 요청을 갱신
+            // 초기 실행 후 10초마다 갱신, 시작 시간 조절
+            const currentSecond = new Date().getSeconds();
+            const startDelay = (10 - (currentSecond % 10)) * 1000;
+            setTimeout(() => {
+                updateWeatherData();
+                setInterval(updateWeatherData, 10000); // 10초마다 요청을 갱신
+            }, startDelay);
         });
     } else {
         console.error("브라우저에서 위치 정보를 가져올 수 없습니다.");
