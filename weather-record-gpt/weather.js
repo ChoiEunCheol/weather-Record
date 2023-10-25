ad89d67b4c9ae4af1bba31b3658aaeb19ef2ac9d
최은철 <wndqhrgkdlel@gmail.com>
2023-10-25 14:34:10 +0900

    현재시각이 10초 단위일 때 업데이트되게 수정 -> 나중에 1시간단위로 바꾸면 됨

--- weather-record-gpt/weather.js
+++ weather-record-gpt/weather.js
@@ -18,44 +18,48 @@ document.addEventListener("DOMContentLoaded", () => {
             const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
             function updateWeatherData() {
-                fetch(apiUrl)
-                    .then((response) => response.json())
-                    .then((data) => {
-                        const weatherInfo = document.getElementById("weather-info");
-                        const temperature = Math.round(data.main.temp - 273.15); // 온도를 섭씨로 변환
-                        const description = data.weather[0].description;
-                        
-                        // 현재 시간을 가져오기
-                        const now = new Date();
-                        const currentTimeInfo = document.getElementById("current-time");
-                        currentTimeInfo.textContent = `갱신 시각: ${now.toLocaleString()}`;
+                const now = new Date();
+                const seconds = now.getSeconds();
 
-                        // 날씨 정보를 업데이트
-                        const weatherText = `현재 온도: ${temperature}°C, 날씨: ${description}`;
-                        weatherInfo.textContent = weatherText;
+                if (seconds % 10 === 0) {
+                    fetch(apiUrl)
+                        .then((response) => response.json())
+                        .then((data) => {
+                            const weatherInfo = document.getElementById("weather-info");
+                            const temperature = Math.round(data.main.temp - 273.15); // 온도를 섭씨로 변환
+                            const description = data.weather[0].description;
+                            
+                            // 현재 시간을 가져오기
+                            const currentTimeInfo = document.getElementById("current-time");
+                            currentTimeInfo.textContent = `갱신 시각: ${now.toLocaleString()}`;
 
-                        // 새로운 날씨 정보를 리스트에 추가
-                        const weatherList = document.getElementById("weather-list");
-                        const li = document.createElement("li");
-                        li.textContent = `갱신 시각: ${now.toLocaleString()}, ${weatherText}`;
+                            // 날씨 정보를 업데이트
+                            const weatherText = `현재 온도: ${temperature}°C, 날씨: ${description}`;
+                            weatherInfo.textContent = weatherText;
 
-                        // 첫 번째 자식으로 추가
-                        if (weatherList.children.length > 0) {
-                            weatherList.insertBefore(li, weatherList.firstChild);
-                        } else {
-                            weatherList.appendChild(li);
-                        }
-                    })
-                    .catch((error) => {
-                        console.error("날씨 정보를 갱신하는 중 오류가 발생했습니다.", error);
-                    });
+                            // 새로운 날씨 정보를 리스트에 추가
+                            const weatherList = document.getElementById("weather-list");
+                            const li = document.createElement("li");
+                            li.textContent = `갱신 시각: ${now.toLocaleString()}, ${weatherText}`;
+
+                            // 첫 번째 자식으로 추가
+                            if (weatherList.children.length > 0) {
+                                weatherList.insertBefore(li, weatherList.firstChild);
+                            } else {
+                                weatherList.appendChild(li);
+                            }
+                        })
+                        .catch((error) => {
+                            console.error("날씨 정보를 갱신하는 중 오류가 발생했습니다.", error);
+                        });
+                }
             }
 
             // 초기 데이터 가져오기
             updateWeatherData();
 
-            // 60초마다 데이터 업데이트
-            setInterval(updateWeatherData, 1000); // 60초마다 요청을 갱신 (1000ms = 1초)
+            // 1초마다 데이터 업데이트
+            setInterval(updateWeatherData, 1000);
         });
     } else {
         console.error("브라우저에서 위치 정보를 가져올 수 없습니다.");
