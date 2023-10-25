405b3708487c10b95874f50dcc1530d8f57bee8d
최은철 <wndqhrgkdlel@gmail.com>
2023-10-25 16:42:21 +0900

    10초 단위로 갱신, 상단에 현재시간 표시 수정

--- weather-record-gpt/weather.js
+++ weather-record-gpt/weather.js
@@ -79,6 +79,11 @@ document.addEventListener("DOMContentLoaded", () => {
                         if (weatherList.children.length >= 168) {
                             weatherList.removeChild(weatherList.lastChild);
                         }
+
+                        // li 태그 추가
+                        const li = document.createElement("li");
+                        li.textContent = `갱신 시각: ${now.toLocaleString()}, ${weatherText}`;
+                        weatherList.insertBefore(li, weatherList.firstChild);
                     })
                     .catch((error) => {
                         console.error("날씨 정보를 갱신하는 중 오류가 발생했습니다.", error);
@@ -92,6 +97,16 @@ document.addEventListener("DOMContentLoaded", () => {
                 updateWeatherData();
                 setInterval(updateWeatherData, 10000); // 10초마다 요청을 갱신
             }, startDelay);
+
+            // 1초마다 시간 갱신
+            function updateCurrentTime() {
+                const currentTimeInfo = document.getElementById("current-time");
+                const now = new Date();
+                currentTimeInfo.textContent = `현재 시각: ${now.toLocaleString()}`;
+            }
+
+            updateCurrentTime();
+            setInterval(updateCurrentTime, 1000); // 1초마다 갱신
         });
     } else {
         console.error("브라우저에서 위치 정보를 가져올 수 없습니다.");
